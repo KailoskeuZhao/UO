219037029d4e8082a558bdf93eb72e950ad18872
KailoskeuZhao <a98165809@gmail.com>
2024-01-05 18:41:42 -0500

    hu

--- UO8/keyRace/kR.cpp
+++ UO8/keyRace/kR.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main(){
-    
+    int s, v1, v2, t1, t2;
+
+    cin >> s >> v1 >> v2 >> t1 >> t2;
+
+    int ft1 = 0, ft2 = 0;
+
+    ft1 = 2 * t1 + (v1 * s);
+    ft2 = 2 * t2 + (v2 * s);
+
+    if(ft1 < ft2){
+        cout << "First";
+    } else if (ft1 == ft2){
+        cout << "Friendship";
+    } else{
+        cout << "Second";
+    }
 }
